786974398b9d8e34c592737199003bc084c9ccdd
tuan2004a <bi03112004@gmail.com>
2025-12-20 16:57:15 +0700

    feat(navbar): add notification button component and refactor imports
    - Create new BtnNotif component for notification button with icon and hover effects
    - Extract notification button logic from Navbar into reusable component
    - Update Navbar imports to use path aliases (@common) for cleaner imports

---
+++ FE-React/src/components/common/button/btnNotif.tsx
@@ -0,0 +1,11 @@
+import { MdNotifications } from "react-icons/md";
+
+const BtnNotif = () => {
+	return (
+		<button className="size-11.5 justify-items-center hover:bg-[#137fec]/10 rounded-xl cursor-pointer">
+			<MdNotifications className="text-2xl" />
+		</button>
+	);
+};
+
+export default BtnNotif;
--- FE-React/src/components/layout/Navbar.tsx
+++ FE-React/src/components/layout/Navbar.tsx
@@ -1,8 +1,8 @@
 import { Avatar, Text } from "@radix-ui/themes";
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { MdNotifications } from "react-icons/md";
-import Search from "../common/input/search";
+import Search from "@common/input/search";
+import BtnNotif from '@common/button/btnNotif';
 
 interface DynamicText {
 	title: string;
@@ -55,12 +55,10 @@ const Navbar = React.memo(() => {
 			</div>
 			<div className="flex">
 				<div className="mr-50">
-					<Search  />
+					<Search />
 				</div>
 				<div className="flex gap-x-4">
-					<button className="size-11.5 justify-items-center hover:bg-[#137fec]/10 rounded-xl cursor-pointer">
-						<MdNotifications className="text-2xl" />
-					</button>
+					<BtnNotif />
 					<div className="flex flex-row-reverse gap-x-3 border-l border-slate-200 pl-4">
 						<Avatar src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop" fallback="A" />
 						<div>
